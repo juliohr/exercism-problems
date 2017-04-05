84d74f80056ec86d49507b2a207331f642916385
Julio <send.to.julio@gmail.com>
2017-04-04 22:48:45 -0300

    bob solution

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,13 +1,24 @@
 module Bob exposing (..)
+
 import Char exposing (isDigit)
+import Regex exposing (regex)
+import String exposing (endsWith, trim, isEmpty, toUpper, toLower)
 
 hey : String -> String
 hey sentence =
-    if (String.toUpper sentence) == sentence then
-        "Whoa, chill out!"
-    else if String.contains "?" sentence then
-        "Sure."
-    else if (String.all isDigit sentence) == True then
-        "Whatever."
-    else
-        "Whatever."
+    if isYelling sentence then "Whoa, chill out!"
+    else if isQuestion sentence then "Sure."
+    else if isSilence sentence then "Fine. Be that way!"
+    else "Whatever."
+
+isYelling : String -> Bool
+isYelling sentence =
+     (toUpper sentence == sentence && toLower sentence /= sentence)
+
+isQuestion : String -> Bool
+isQuestion sentence =
+    endsWith "?" sentence
+
+isSilence : String -> Bool
+isSilence sentence =
+    trim sentence |> isEmpty
